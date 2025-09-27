6a276bcc2fc99930688deccfdbdb739f6f7146a0
unschlagbar <153451111+unschlagbar@users.noreply.github.com>
2025-09-27 14:27:39 +0200

    tzh

--- src/ui/ui_events.rs
+++ src/ui/ui_events.rs
@@ -1,4 +1,4 @@
-use winit::event::MouseScrollDelta;
+use winit::event::{ElementState, MouseScrollDelta, TouchPhase};
 
 #[derive(Debug, Clone, Copy)]
 pub enum UiEvent {
@@ -7,4 +7,23 @@ pub enum UiEvent {
     Move,
     Scroll(MouseScrollDelta),
     Tick,
+}
+
+impl From<TouchPhase> for UiEvent {
+    fn from(value: TouchPhase) -> Self {
+        match value {
+            TouchPhase::Started => Self::Press,
+            TouchPhase::Moved => Self::Move,
+            TouchPhase::Ended | TouchPhase::Cancelled => Self::Release,
+        }
+    }
+}
+
+impl From<ElementState> for UiEvent {
+    fn from(value: ElementState) -> Self {
+        match value {
+            ElementState::Pressed => Self::Press,
+            ElementState::Released => Self::Release,
+        }
+    }
 }
